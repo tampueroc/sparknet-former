c7ec607bc05bd40321abf10324975c0b36d3c585
Tomás Ampuero <tom.ampuero.c@gmail.com>
2025-01-19 11:48:59 -0600

    fix: add init

--- models/transformers/__init__.py
+++ models/transformers/__init__.py
@@ -1,9 +1,13 @@
 from .temporal_transformer import TemporalTransformerEncoder
 from .positional_encoders import SinusoidalPositionalEncoding, LearnablePositionalEncoding, RelativePositionalEncoding
+from .patch_positional_encoders import Learnable2DPositionalEncoding
+from .patch_embedding import PatchEmbedding
 
 __all__ = [
         "TemporalTransformerEncoder",
         "SinusoidalPositionalEncoding",
         "LearnablePositionalEncoding",
-        "RelativePositionalEncoding"
+        "RelativePositionalEncoding",
+        "Learnable2DPositionalEncoding",
+        "PatchEmbedding"
 ]
