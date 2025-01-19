6264782649b9b3b139eac46bf5fcfff3acc18445
Tomás Ampuero <tom.ampuero.c@gmail.com>
2025-01-19 15:56:35 -0600

    fix: metrics callback

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,11 @@
 from .checkpoints import CheckpointHandler
 from .logger import Logger
 from .early_stopping import EarlyStoppingHandler
+from .metrics_callback import MetricsLoggingCallback
 
 __all__ = [
         "CheckpointHandler",
         "Logger",
-        "EarlyStoppingHandler"
+        "EarlyStoppingHandler",
+        "MetricsLoggingCallback"
 ]
