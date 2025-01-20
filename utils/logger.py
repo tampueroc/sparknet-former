c8a5f42f1e1e7e9c64879660f66d942f1cbcfbd1
Tomás Ampuero <tom.ampuero.c@gmail.com>
2025-01-19 20:19:50 -0600

    fix: add log graph

--- utils/logger.py
+++ utils/logger.py
@@ -5,14 +5,15 @@ class Logger:
     Manages logging with TensorBoard.
     """
     @staticmethod
-    def get_tensorboard_logger(save_dir, name="default"):
+    def get_tensorboard_logger(save_dir, name="default", log_graph=True):
         """
         Creates a TensorBoard logger.
         Args:
             save_dir (str): Directory where logs will be saved.
             name (str): Name of the experiment.
+            log_graph (bool): Adds the computational graph to tensorboard
         Returns:
             TensorBoardLogger: Configured TensorBoard logger.
         """
-        return TensorBoardLogger(save_dir=save_dir, name=name)
+        return TensorBoardLogger(save_dir=save_dir, name=name, log_graph=True)
 
